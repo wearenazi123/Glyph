9600489d422a85401e9b729605c1b79f292e65c8
wearenazi123@126.com <wearenazi123@126.com>
2022-03-12 11:51:44 +0800

    Glyph

---
+++ src/com/company/Glyph.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Glyph {
+    void draw(){System.out.println("Glyph.draw()");}
+    Glyph(){
+        System.out.println("Glyph() before draw()");
+        draw();
+        System.out.println("Glyph() after draw");
+    }
+
+}
